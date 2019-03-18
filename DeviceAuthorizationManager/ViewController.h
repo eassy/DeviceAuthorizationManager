c03b14b9f29a81a4d035e92f825619a933ac5c0b
houwenjie <houwenjie11@gmail.com>
2019-03-18 16:06:33 +0800

    Initial Commit

---
+++ DeviceAuthorizationManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceAuthorizationManager
+//
+//  Created by houwenjie on 2019/3/18.
+//  Copyright © 2019 houwenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeviceAuthorizationManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DeviceAuthorizationManager
+//
+//  Created by houwenjie on 2019/3/18.
+//  Copyright © 2019 houwenjie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
